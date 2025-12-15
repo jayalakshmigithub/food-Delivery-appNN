6ca526c0fd6d4a447d3b7c55703529002fbc27d2
Your Name <lakshmijaya2912@gmail.com>
2025-12-15 11:53:04 +0530

    [fix] path fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import Categories from "./pages/allCategories.jsx";
+import Categories from "./pages/allCategories";
 import Cart from "./components/Cart/Cart";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home";
 
 function App() {
   return (
