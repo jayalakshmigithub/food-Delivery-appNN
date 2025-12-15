d5f406dec326ccce23b10285fa08954a1df76bc3
Your Name <lakshmijaya2912@gmail.com>
2025-12-15 11:45:58 +0530

    [fix] src path fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import { useState ,useEffect} from 'react'
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import Categories from './pages/allCategories.jsx';
-import Cart from './components/Cart/Cart';
-import Home from './pages/Home.jsx';
-
+import Layout from "./components/Layout/Layout";
+import Categories from "./pages/allCategories.jsx";
+import Cart from "./components/Cart/Cart";
+import Home from "./pages/Home.jsx";
 
 function App() {
- 
-
   return (
-     <BrowserRouter>
+    <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/cart" element={<Cart />} />
-          
         </Routes>
       </Layout>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
